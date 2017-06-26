e2cd861d85ac4067a20fa26254b25b90779998c9
Jonathan Ansley <ansley.jonathan.e@gmail.com>
2017-06-25 20:13:58 -0400

    not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,197 +1,118 @@
 // This code will run as soon as the page loads
-// window.onload = function() {
-//   // alert('JavaScript file - window.onload');
+window.onload = function() {
 
-//   };
+  alert('JavaScript file - window.onload');
 
-// <button type="button" class="btn btn-success">answer 1</button>
-
-// <button type="button" class="btn btn-primary" id = "done-button">Done</button>
-
-$('#start-button').on('click', function() {
-  console.log('You clicked #start-button');
-
-  //after user clicks start button
-  $('#start-button').remove();
-
-  // Append each question to subwrapper.
-  for (var i = questions.length - 1; i >= 0; i--) {
-    $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
-
-      // Storing values in with buttons
-      for (var j = questions[i].answers.length - 1; i >= 0; i--) {
-      
-        $('#subwrapper').append("<input type = 'radio' name = 'question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
-
-      } // end of sub-for loop
-
-  } // end of for loop
-
-  }); // end of start button listener
-
-$(document).on('click', '#end', function(){
-  game.done();
- });
-
-var questions = [
-  {
-  question: 'Who said, Of all the souls I have encountered in my travels, his was the most....human.',
-  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
-  correctAnswer: 'James T. Kirk'
-  },
-
-  {
-  question: 'Who said, \"We\'re not here to take over. We just want to sell you things.\"',
-  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
-  correctAnswer: 'Quark the Ferengi'
-  },
-
-  {
-  question: 'Having is not so pleasing a thing as wanting, it may not be logical but it is often true.',
-  answers:[ 'Mr. Sulu', 'Lt. Uhura', 'Number One' ],
-  correctAnswer: 'Mr. Spock'
-  },
-
-  {
-  question: 'Jim (Captain Kirk), you don\’t go around asking the Almighty for his I.D.!',
-  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
-  correctAnswer: 'Dr ‘Bones’ McCoy'
-  },
-
-  {
-  question: 'I\'m a doctor, not a bricklayer',
-  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
-  correctAnswer: 'Dr ‘Bones’ McCoy'
-  },
-
-  {
-  question: 'Which series has vocal music with the opening theme?',
-  answers:[ 'Voyager', 'Deep Space Nine', 'Next Generation' ],
-  correctAnswer: 'Enterprise'
-  },
-
-  {
-  question: 'EMH is another word for which character in Voyager?',
-  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
-  correctAnswer: 'The Doctor'
-  },
-
-  {
-  question: 'Which quadrant is Voyager trying to leave?',
-  answers:[ 'Alpha', 'Beta', 'Borg' ],
-  correctAnswer: 'Delta'
-  }
-
-  ];
+};
 
 
-var game = {
-  correctAnswerCount: 0,
-  incorrectAnswerCount: 0,
 
-  timeCounter: 20,
-  // timeCounter: 120;
 
-  countDown: function(){
-    game.timeCounter--;
-    $('#counter').html(game.timeCounter);
-    if (game.timeCounter <= 0)
-    {
-      console.log('out of time');
-      game.done();
-    }
-  }, //end of countDown function
+// 1.  Fool around with the GIPHY API. [Giphy API](https://github.com/Giphy).
+//    * Be sure to read about these GIPHY parameters (hint, hint): 
+//      * `q`
+//      * `limit`
+//      * `rating`
+//    * Like many APIs, GIPHY requires developers to use a key to access their API data.
+// For now, you can use their [public API key](https://github.com/Giphy/GiphyAPI#public-beta-key).
+//    * Make sure you switch the protocol in the query URL from **`http to https`**, or the app may not work properly when deployed to Github Pages.
 
-  start: function() {
-    console.log('start function');
-  },
 
-  // Determine if input type is checked.
-  done: function(){
 
-    $.each($('input[name="question-0]":checked'), function(){
-      if($(this).val()===questions[0].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
 
-    $.each($('input[name="question-1]":checked'), function(){
-      if($(this).val()===questions[1].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
+// 2. Your app should take the topics in this array and create buttons in your HTML.
+//    * Try using a loop that appends a button for each string in the array.
 
-    $.each($('input[name="question-2]":checked'), function(){
-      if($(this).val()===questions[2].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
 
-    $.each($('input[name="question-3]":checked'), function(){
-      if($(this).val()===questions[3].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
 
-    $.each($('input[name="question-4]":checked'), function(){
-      if($(this).val()===questions[4].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
 
-    $.each($('input[name="question-5]":checked'), function(){
-      if($(this).val()===questions[5].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
+// 3. When the user clicks on a button, the page should grab 10 static,
+// non-animated gif images from the GIPHY API and place them on the page. 
 
-   $.each($('input[name="question-6]":checked'), function(){
-      if($(this).val()===questions[6].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
 
-  $.each($('input[name="question-7]":checked'), function(){
-      if($(this).val()===questions[7].correctAnswer){
-        game.correctAnswerCount++;
-      } else {
-        game.incorrectAnswerCount++;
-      }
-    })
 
-}, // end of done function
 
-// Show results of game.
-result: function(){
-  clearInterval(timer);
+// 4. When the user clicks one of the still GIPHY images, the gif should animate.
+// If the user clicks the gif again, it should stop playing.
 
-  // Remove timer.
-  $('#subwrapper h2').remove();
 
-  // 
-  $('#subwrapper').html("<h2>all done</h2>")
 
-  $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
-  $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
 
-  // Calculate and show unanswered
-  $('#subwrapper').append("<h3>Unanswered: "+(questions.length - (this.incorrectAnswerCount + this.correctAnswerCount))+"</h3>");
-} // end of result function
+// 5. Under every gif, display its rating (PG, G, so on). 
+//    * This data is provided by the GIPHY API.
 
-}
 
-game.result();
+
+
+
+
+
+//    * Only once you get images displaying with button presses
+// should you move on to the next step.
+
+
+
+
+// 6. Add a form to your page takes the value from a user input box
+// and adds it into your `topics` array.
+// Then make a function call that takes each topic in the array remakes
+// the buttons on the page.
+
+
+
+
+// original buttons
+var topics = [ 'Star Trek', 'Captain Kirk', 'Khan' ];
+
+// GIF parameters
+var gifLimit = 10;
+var gifRating = 'G';
+
+    // Event listener for our buttons
+    $("#gif-button").on("click", function() {
+
+      // Storing our giphy API URL for a random gif image
+      var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=trek";
+
+      // Perfoming an AJAX GET request to our queryURL
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      // After the data from the AJAX request comes back
+      .done(function(response) {
+
+        console.log(response);
+
+        for (var i = response.data.length - 1; i >= 0; i--) {
+          response.data[i]
+          var gifDiv = $('<div>')
+          var p = $('<p>').text('Rating: '+ response.data[i].rating);
+
+          // Creating and storing an image tag
+          var gifImage = $('<img>');
+          gifImage.attr('src', response.data[i].images.fixed_height_url)
+
+          gifDiv.append(p);
+
+          gifDiv.append(gifImage);
+
+          // Prepending the gifImage to the images div
+          $('#images').prepend(gifImage);
+
+        }
+
+        // Saving the image_original_url property
+        var imageUrl = response.data.image_original_url;
+
+        // Saving the image's rating property
+        var imageRating = response.data.rating;
+
+        // Setting the gifImage src attribute to imageUrl
+        gifImage.attr("src", imageUrl);
+        gifImage.attr("alt", "gif image");
+
+        
+      });
+    });
